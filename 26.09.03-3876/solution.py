701c40789b9f48c315636dae390cd405e7ea487e
dudco <dudco1129@naver.com>
2026-09-03 19:13:08 +0900

    9/3 문제 풀이

--- 26.09.03-3876/solution.py
+++ 26.09.03-3876/solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        pass
+        minOdd = float("inf")
+        minEven = float("inf")
+        for i in nums1:
+            if i % 2 == 0 and minEven > i: minEven = i
+            elif i % 2 != 0 and minOdd > i: minOdd = i
+
+        if minOdd == float("inf") or minEven == float("inf"): return True
+        return minEven > minOdd
