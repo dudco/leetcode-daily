3a0bd8282d53f1cee4c9bf90a6fefb0e667af809
dudco <dudco1129@naver.com>
2026-09-04 09:35:57 +0900

    9/4 문제풀이

--- 26.09.04-3903/solution.py
+++ 26.09.04-3903/solution.py
@@ -1,3 +1,10 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
-        pass
+        l = nums[0]
+
+        for idx, n in enumerate(nums):
+            if n > l: l = n
+
+            s = min(nums[idx:])
+            if l - s <= k: return idx
+        return -1
