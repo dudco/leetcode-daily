ae302ad99780b9ac2945d2407e9586d74c8205d5
dudco <dudco1129@naver.com>
2026-09-09 22:58:09 +0900

    9/9 문제풀이

--- 26.09.09-3871/test.py
+++ 26.09.09-3871/test.py
@@ -4,13 +4,21 @@ import solution
 
 
 class SolutionTests(unittest.TestCase):
-    def test_case1(self):
-        s = solution.Solution()
-        self.assertEqual(s.countCommas(1002), 3)
+    # def test_case1(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.countCommas(1002), 3)
 
-    def test_case2(self):
+    # def test_case2(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.countCommas(998), 0)
+
+    def test_case3(self):
         s = solution.Solution()
-        self.assertEqual(s.countCommas(998), 0)
+        self.assertEqual(s.countCommas(1004590), 1008182)
+
+    def test_case4(self):
+        s = solution.Solution()
+        self.assertEqual(s.countCommas(1122872257463), 3490488028856)
 
 if __name__ == '__main__':
     unittest.main()
