9576283b2788063b48add3da40d4fc330a3fc208
dudco <dudco1129@naver.com>
2026-09-14 02:13:06 +0900

    9/13 문제풀이

--- 26.09.13-835/solution.py
+++ 26.09.13-835/solution.py
@@ -1,3 +1,97 @@
 class Solution:
     def largestOverlap(self, img1: list[list[int]], img2: list[list[int]]) -> int:
-        pass
+        n = len(img1)
+        def make_bit(image: list[list[int]]):
+            b = [0] * n
+            
+            for idx in range(n):
+                for ei in range(n):
+                    b[idx] += image[idx][n-ei-1] * (2 ** ei)
+
+            return b
+
+        def check(image1: list[int], image2: list[int]):
+            cnt = 0
+            for idx in range(n):
+                cnt += (image1[idx] & image2[idx]).bit_count()
+
+            return cnt
+
+        def move_right(image: list[int]):
+            r = image.copy()
+            for idx in range(n):
+                r[idx] = r[idx] >> 1
+
+            return r
+
+        def move_down(image: list[int]):
+            d = image.copy()
+            d = [0] + d[:n-1]
+            return d
+
+        def move_left(image: list[int]):
+            l = image.copy()
+            for idx in range(n):
+                l[idx] = (l[idx] << 1) & ((2 ** n) - 1)
+
+            return l
+
+        def move_up(image: list[int]):
+            u = image.copy()
+            u = u[1:] + [0]
+            return u
+                        
+
+        b_img1 = make_bit(img1)
+        b_img2 = make_bit(img2)
+        ret = check(b_img1, b_img2)
+
+        b_img = b_img1.copy()
+        for _ in range(n+1): # 30
+            img = b_img.copy()
+            c = check(img, b_img2) # 30
+            ret = max(ret, c)
+            
+            for _ in range(n+1): # 30
+                img = move_right(img) # 30
+                c = check(img, b_img2) # 30
+                ret = max(ret, c)
+            b_img = move_down(b_img) 
+
+        b_img = b_img1.copy()
+        for _ in range(n+1): # 30
+            img = b_img.copy()
+            c = check(img, b_img2) # 30
+            ret = max(ret, c)
+            
+            for _ in range(n+1): # 30
+                img = move_right(img) # 30
+                c = check(img, b_img2) # 30
+                ret = max(ret, c)
+            b_img = move_up(b_img) 
+
+        b_img = b_img1.copy()
+        for _ in range(n+1): # 30
+            img = b_img.copy()
+            c = check(img, b_img2) # 30
+            ret = max(ret, c)
+            
+            for _ in range(n+1): # 30
+                img = move_left(img) # 30
+                c = check(img, b_img2) # 30
+                ret = max(ret, c)
+            b_img = move_down(b_img) 
+
+        b_img = b_img1.copy()
+        for _ in range(n+1): # 30
+            img = b_img.copy()
+            c = check(img, b_img2) # 30
+            ret = max(ret, c)
+            
+            for _ in range(n+1): # 30
+                img = move_left(img) # 30
+                c = check(img, b_img2) # 30
+                ret = max(ret, c)
+            b_img = move_up(b_img) 
+
+        return ret
--- 26.09.13-835/test.py
+++ 26.09.13-835/test.py
@@ -4,17 +4,43 @@ import solution
 
 
 class SolutionTests(unittest.TestCase):
-    def test_case1(self):
-        s = solution.Solution()
-        self.assertEqual(s.largestOverlap([[1, 1, 0], [0, 1, 0], [0, 1, 0]], [[0, 0, 0], [0, 1, 1], [0, 0, 1]]), 3)
+    # def test_case1(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.largestOverlap([[1, 1, 0], [0, 1, 0], [0, 1, 0]], [[0, 0, 0], [0, 1, 1], [0, 0, 1]]), 3)
 
-    def test_case2(self):
-        s = solution.Solution()
-        self.assertEqual(s.largestOverlap([[1]], [[1]]), 1)
+    # def test_case2(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.largestOverlap([[1]], [[1]]), 1)
 
-    def test_case3(self):
+    # def test_case3(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.largestOverlap([[0]], [[0]]), 0)
+
+    # def test_case4(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.largestOverlap([[0,0,0,0,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]], [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,0]]), 1)
+
+    # def test_case5(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.largestOverlap([[1,0],[0,0]], [[0,1],[1,0]]), 1)
+    
+    # def test_case6(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.largestOverlap([[1,1,1,1],[1,1,0,1],[1,1,1,0],[0,1,0,1]], [[1,1,1,0],[1,1,1,1],[1,1,1,0],[1,0,0,0]]), 9)
+
+    # def test_case7(self):
+    #     s = solution.Solution()
+    #     self.assertEqual(s.largestOverlap(
+    #         [[1,0,1,1,0,1,0,0,1,1],[1,1,1,0,1,1,1,1,1,1],[1,1,1,0,1,1,1,1,1,1],[1,1,1,0,1,1,1,1,1,1],[1,1,1,1,0,1,0,0,1,0],[1,1,1,1,1,1,1,1,0,0],[1,0,1,1,0,1,1,0,0,1],[1,0,1,0,1,1,1,1,1,1],[1,1,1,1,1,0,0,1,1,0],[1,1,0,1,1,0,1,0,1,1]],
+    #         [[1,1,0,1,1,1,1,1,1,1],[1,0,0,0,0,1,0,1,1,0],[1,1,1,1,0,1,1,1,1,1],[1,0,0,1,1,1,1,1,0,1],[1,1,1,1,1,0,1,1,0,0],[1,1,1,1,0,1,0,1,1,1],[0,1,1,1,1,0,1,1,1,1],[1,0,1,1,1,0,1,1,0,1],[1,0,1,1,1,0,1,0,0,0],[1,1,1,1,1,1,1,0,1,0]]
+    #     ), 53)
+
+    def test_case8(self):
         s = solution.Solution()
-        self.assertEqual(s.largestOverlap([[0]], [[0]]), 0)
+        self.assertEqual(s.largestOverlap(
+            [[0,0,0,0,0],[0,0,0,1,0],[0,0,0,1,0],[0,1,1,0,0],[0,0,0,1,0]],
+            [[0,0,0,1,0],[0,0,0,0,0],[1,1,1,0,1],[0,0,1,1,1],[0,1,0,0,0]]
+        ), 4)
 
 if __name__ == '__main__':
     unittest.main()
